5494e7a99c948dd98b61fcaca43b556d2a2efbd1
Koushik <koushik.k@fireflink.com>
2023-04-26 12:11:39 +0530

    koushik

---
+++ src/main/java/com/example/FirstConflict/FirstConflictApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstConflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstConflictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstConflictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstConflict/FirstConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstConflict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
