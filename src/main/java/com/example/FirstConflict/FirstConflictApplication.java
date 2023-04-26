6b9214dc1abadbb7944673f77e475cec282343ec
ningaraj <ningarajmeti1999@gmail.com>
2023-04-26 12:45:44 +0530

    change

--- src/main/java/com/example/FirstConflict/FirstConflictApplication.java
+++ src/main/java/com/example/FirstConflict/FirstConflictApplication.java
@@ -9,8 +9,7 @@ public class FirstConflictApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstConflictApplication.class, args);
 		System.out.println("hi koushik hello");
-
-		System.out.println("hello");
+		System.out.println("bye");
 	}
 
 }
